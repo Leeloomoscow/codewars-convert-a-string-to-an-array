10057456e22363f3011d19930bd3e821baad19ab
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-18 20:30:23 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,19 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void basicTests() {
+        assertArrayEquals(new String[]{"Robin", "Singh"}, Solution.stringToArray("Robin Singh"));
+        assertArrayEquals(new String[]{"I", "love", "arrays", "they", "are", "my", "favorite"}, Solution.stringToArray("I love arrays they are my favorite"));
+    }
 
+    private static class Solution {
+        public static Object[] stringToArray(String s) {
+            return s.split("\\W+");
+        }
     }
 }
+
+// \\W+ будет соответствовать всем неалфавитным символам, встречающимся один или несколько раз. Поэтому нет необходимости заменять
